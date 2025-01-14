9e5314ef18042d27fdf13f60e7600876f8675814
Bibidh <bibidhrstha@icloud.com>
2025-01-14 10:47:27 +0545

    Added Codes to GradeCalculator

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -1,5 +1,26 @@
 package Workshop8;
+import java.util.Scanner;
 
 public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        String studentName;
+        double grade;
 
+        System.out.print("Enter student's name: ");
+        studentName = scanner.nextLine();
+        
+        System.out.print("Enter grade for " + studentName + ": ");
+        grade = scanner.nextDouble();
+
+        if (grade >= 40) {
+            System.out.println(studentName + " has passed with a grade of " + grade);
+        } else {
+            System.out.println(studentName + " has failed with a grade of " + grade);
+        }
+        
+        scanner.close();
+    }
 }
+
